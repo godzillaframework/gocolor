b55cfd90767da927d2750c1cec1598f065c7195a
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-24 09:22:46 +0530

    gocolor testing

--- gocolor_test.go
+++ gocolor_test.go
@@ -1,10 +1,20 @@
 package gocolor_test
 
 import (
-	"fmt"
+	coloring "github.com/godzillaframework/gocolor"
 	"testing"
 )
 
+func testPrint() {
+	coloring.Print("Hello").In("green")
+}
+
 func tests(t *testing.T) {
-	fmt.Println("Test")
+	str := "WAIT WHY THIS CODE LOOKS GOOD??"
+
+	expected := "\033[32m EXPECTED \033[0m\n"
+
+	if str != expected {
+		t.Error("Expected", expected)
+	}
 }
